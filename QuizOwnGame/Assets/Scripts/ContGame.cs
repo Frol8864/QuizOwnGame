d79c508c53a621ebf3ec93683ff5957dfe738ea6
Alex Frolov <frolov8864@gmail.com>
2024-01-06 15:10:51 +0300

    чутка

---
+++ QuizOwnGame/Assets/Scripts/ContGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+//using System;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+
+public class ContGame : MonoBehaviour
+{
+    
+}
